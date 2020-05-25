b70cff32704f76c5bea5c1c99c1f74570abaf947
Artur Quaresma <artursquaresma@gmail.com>
2020-05-25 14:53:08 +0100

    Metodo para obter todos as cenas ussei firebase asdsa

---
+++ XamarinFirebase Retrieve/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program().Run().Wait();
+        }
+
+        private async Task Run()
+        {
+            // Since the dinosaur-facts repo no longer works, populate your own one with sample data
+            // in "sample.json"
+            var firebase = new FirebaseClient("https://trofimon-pap.firebaseio.com/");
+
+            var receitas = await firebase
+                     .Child("Receitas")
+                     .Child("12")
+                     .OrderByKey()
+                     .OnceAsync<Receitas>();
+
+            foreach (var receita in receitas)
+            {
+                Console.WriteLine($"{receita.Key} is {receita.Object.NomeReceita}m high.");
+            }
+
+            Console.WriteLine("ok");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ XamarinFirebase Retrieve/ConsoleApp1/Receitas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Receitas
+    {
+        public string NomeReceita { get; set; }
+        public string Ingredientes { get; set; }
+        public string Preparacao { get; set; }
+        public string Comentarios { get; set; }
+        public string Imagem { get; set; }
+        public bool Privacidade { get; set; }
+    }
+}
