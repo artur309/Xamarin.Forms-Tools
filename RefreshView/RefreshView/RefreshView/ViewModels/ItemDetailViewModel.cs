c32989678ffc56c3bad3c7f4a112e792ce7851e6
Artur Quaresma <artursquaresma@gmail.com>
2020-05-31 12:35:29 +0100

    RefreshView code

---
+++ RefreshView/RefreshView/RefreshView/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RefreshView.Models;
+
+namespace RefreshView.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ RefreshView/RefreshView/RefreshView/ViewModels/MyViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RefreshView.ViewModels
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        public ICommand RefreshCommand { protected set; get; }
+        public ObservableCollection<string> Items { get; set; }
+
+
+        bool _isRefreshing = false;
+        public bool IsRefreshing
+        {
+            get
+            {
+                return _isRefreshing;
+            }
+
+            set
+            {
+                if (_isRefreshing != value)
+                {
+                    _isRefreshing = value;
+                    OnPropertyChanged("IsRefreshing");
+
+                }
+            }
+
+        }
+        public MyViewModel()
+        {
+            Items = new ObservableCollection<string>();
+
+            Items.Add("ddddd");
+            Items.Add("ccccc");
+            Items.Add("eeeeee");
+
+            RefreshCommand = new Command<string>((key) =>
+            {
+                Items.Add("ffffff");
+                IsRefreshing = false;
+            });
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
